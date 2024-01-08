5a53aef585f775a82401c1897671fca1768c1368
Grace Chen Abudi <cheezumcali23@gmail.com>
2024-01-08 22:19:44 +0200

    feat(players): Added players name and symbol to game board

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import Player from "./components/Player";
+
 function App() {
-  return <p>Coming Soon...</p>;
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Player 1" symbol="X" />
+          <Player name="Player 2" symbol="O" />
+        </ol>
+        Game Board
+      </div>
+      Log
+    </main>
+  );
 }
 
 export default App;
