5256e296f47bdaa29d9b1d035911cfa098cc5930
Grace Chen Abudi <98914366+Chen-Abudi@users.noreply.github.com>
2024-01-22 00:07:58 +0200

    feat(log): Add log turns, deriving and sharing states across components (#4)

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
 import { useState } from "react";
 
-import Player from "./components/Player";
-import GameBoard from "./components/GameBoard";
+import Player from "./components/Player.jsx";
+import GameBoard from "./components/GameBoard.jsx";
+import Log from "./components/Log.jsx";
 
 function App() {
+  const [gameTurns, setGameTurns] = useState([]);
   const [activePlayer, setActivePlayer] = useState("X");
 
-  function handleSelectSquare() {
+  function handleSelectSquare(rowIndex, colIndex) {
     setActivePlayer((currentActivePlayer) =>
       currentActivePlayer === "X" ? "O" : "X"
     );
+    setGameTurns((prevTurns) => {
+      let currentPlayer = "X";
+
+      if (prevTurns.length > 0 && prevTurns[0].player === "X") {
+        currentPlayer = "O";
+      }
+
+      const updateTurns = [
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+        ...prevTurns,
+      ];
+
+      return updateTurns;
+    });
   }
 
   return (
@@ -27,12 +43,9 @@ function App() {
             isActive={activePlayer === "O"}
           />
         </ol>
-        <GameBoard
-          onSelectSquare={handleSelectSquare}
-          activePlayerSymbol={activePlayer}
-        />
+        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
       </div>
-      Log
+      <Log turns={gameTurns} />
     </main>
   );
 }
